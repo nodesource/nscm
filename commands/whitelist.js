84584ae6268b31c107c321304116e22d2256099b
Nathan White <nw@nwhite.net>
2017-03-15 22:16:34 -0600

    package fixes

--- commands/whitelist.js
+++ commands/whitelist.js
@@ -4,8 +4,8 @@ const debug = require('debug')('nscm:whitelist')
 const url = require('url')
 const Table = require('cli-table')
 const inquirer = require('inquirer')
-const async = require('async')
-const request = require('request')
+const eachLimit = require('async.eachlimit')
+const request = require('client-request')
 const chalk = require('chalk')
 const tools = require('../lib/tools')
 const log = require('../lib/logger')
@@ -69,7 +69,7 @@ function addWhitelist (opts, callback) {
   const isCallback = typeof callback === 'function'
 
   let success = []
-  async.eachLimit(opts.packages, opts.concurrency, (pkg, next) => {
+  eachLimit(opts.packages, opts.concurrency, (pkg, next) => {
     debug(`adding ${pkg.name} to the whitelist`)
     request({
       uri: url.resolve(opts.registry, '/api/v1/whitelist'),
@@ -271,7 +271,7 @@ function reset (name, sub, opts, callback) {
           return
         }
 
-        async.eachLimit(whitelist, opts.concurrency, (pkg, next) => {
+        eachLimit(whitelist, opts.concurrency, (pkg, next) => {
           deletePackage(Object.assign(opts, { package: `${pkg.name}@${pkg.version}` }), next)
         }, err => {
           if (err) {
