c8adf77e8771de85b41b0688217f951073059902
Max Harris <max@nodesource.com>
2017-05-04 22:22:48 -0700

    treat whitespace and comments kindly in rc files

--- tests/rc-test.js
+++ tests/rc-test.js
@@ -6,16 +6,17 @@ const path = require('path')
 const commentChar = '#'
 
 const rawData = fs.readFileSync(path.join(__dirname, 'fixtures', 'npmrc'), 'utf-8')
-const parsedData = {
-  'init.author.name': { value: 'Max Harris', comment: false },
-  'init.author.email': { value: 'harris.max@gmail.com', comment: false },
-  'init.author.url': { value: 'http://maxharris.org/', comment: false },
-  'email': { value: 'npm@nodesource.com', comment: false },
-  'registry.npmjs.org/:_authToken': { value: 'ab01234c-5678-901d-2345-e67f8g901hij', comment: false },
-  'progress': { value: 'false', comment: false },
-  'commentedkey': { value: 'value', comment: true },
-  '//notacommentjustaprotocolfreeurl.com': { value: 'chompinonkalelikeagiraffe', comment: false }
-}
+const parsedData = [
+  { key: 'init.author.name', value: 'Max Harris', comment: false },
+  { key: 'init.author.email', value: 'harris.max@gmail.com', comment: false },
+  { key: 'init.author.url', value: 'http://maxharris.org/', comment: false },
+  { key: 'email', value: 'npm@nodesource.com', comment: false },
+  { key: 'registry.npmjs.org/:_authToken', value: 'ab01234c-5678-901d-2345-e67f8g901hij', comment: false },
+  { key: 'progress', value: 'false', comment: false },
+  { key: 'commentedkey', value: 'value', comment: true },
+  { key: '//notacommentjustaprotocolfreeurl.com', value: 'chompinonkalelikeagiraffe', comment: false },
+  { key: '', value: '', comment: false }
+]
 
 test('parse rc file contents', t => {
   const actual = parse(rawData, commentChar)
@@ -29,15 +30,9 @@ test('stringify rc file contents', t => {
   t.end()
 })
 
-test('parse rc file contents, with empty lines', t => {
-  const actual = parse(rawData + '\n\n\n', commentChar)
-  t.deepEquals(actual, parsedData, 'parsed rc')
-  t.end()
-})
-
 test('parse rc file contents, with empty key', t => {
   const actual = parse(rawData + '\nfoo=', commentChar)
-  const expected = Object.assign({}, parsedData, { foo: { comment: false, value: '' } })
+  const expected = parsedData.concat({ key: 'foo', comment: false, value: '' })
   t.deepEquals(actual, expected, 'parsed rc')
   t.end()
 })
@@ -51,5 +46,38 @@ test('parse rc file contents, with empty value', t => {
 test('parse rc file contents, with empty key and empty value', t => {
   const actual = parse(rawData + '\n=', commentChar)
   t.deepEquals(actual, parsedData, 'parsed rc')
+  t.end()
+})
+
+const rawPmuellrData = fs.readFileSync(path.join(__dirname, 'fixtures', 'pmuellr'), 'utf-8')
+const parsedPmuellrData = [
+  { key: ' must be mode 0600; eg `chmod 0600 .npmrc`', value: '', comment: true },
+  { key: '', value: '', comment: false },
+  { key: 'cache', value: '~/.npm-ncm', comment: false },
+  { key: ' always-auth', value: 'true', comment: true },
+  { key: '', value: '', comment: false },
+  { key: ' registry', value: 'https://registry.npmjs.org/', comment: true },
+  { key: ' registry', value: 'https://foo.registry.nodesource.io', comment: true },
+  { key: ' registry', value: 'http://localhost:3002', comment: true },
+  { key: '', value: '', comment: false },
+  { key: ' pmuellr@nodesource.com', value: '', comment: true },
+  { key: ' registry', value: 'https://xxxxxxxxxxxxxxxxxxxx.registry.nodesource.io/', comment: true },
+  { key: '', value: '', comment: false },
+  { key: ' pmuellr@apache.org', value: '', comment: true },
+  { key: ' registry', value: 'https://yyyyyyyyyyyyyyyyyyyy.registry.nodesource.io/', comment: true },
+  { key: '', value: '', comment: false },
+  { key: 'registry', value: 'https://yyyyyyyyyyyyyyyyyyyy.registry.nodesource.io/', comment: false },
+  { key: '', value: '', comment: false }
+]
+
+test('parse pmuellr\'s rc file contents', t => {
+  const actual = parse(rawPmuellrData, commentChar)
+  t.deepEquals(actual, parsedPmuellrData, 'parsed pmuellr\'s rc')
+  t.end()
+})
+
+test('stringify pmuellr\'s parsed rc file', t => {
+  const actual = stringify(parsedPmuellrData, commentChar)
+  t.equals(actual, rawPmuellrData, 'stringified pmuellr\'s rc')
   t.end()
 })
